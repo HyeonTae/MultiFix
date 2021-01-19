465a3bb7c274072ec729c2fdec831f5a66f7e17c
Hyeontae <dchs504@gmail.com>
2021-01-20 04:16:47 +0900

    data generator update

--- data_processing/DrRepair_spoc/data_generator.py
+++ data_processing/DrRepair_spoc/data_generator.py
@@ -1,5 +1,5 @@
 from util.helpers import apply_fix, getTrace, getEditDistance, apply_edits
-from util.helpers import cpp_compilation_errors, make_dir_if_not_exists
+from util.helpers import cpp_compilation_errors, make_dir_if_not_exists, tokens_to_source
 from util.c_tokenizer import C_Tokenizer
 import os
 import time
@@ -49,25 +49,25 @@ def generate_training_data(path, validation_users):
     raw_data_path = os.path.join(path, 'raw_data', 'spoc-train.tsv')
     raw_data = pd.read_csv(raw_data_path, delimiter='\t', header=0)
 
-    exceptions_in_mutate_call = 0
+    exceptions_in_mutate_call = list()
     check_problem_path = os.path.join(path, 'comp_result')
     make_dir_if_not_exists(check_problem_path)
 
     count = 0
-    _error = 0
+    _error = list()
+    _token_error = list()
     large_token = 0
     result = {'train': {}, 'validation': {}}
-
-    exceptions_in_mutate_call = 0
 
     for data_file in tqdm(glob.glob(data_path+"*")):
         count += 1
         try:
             data = json.loads(open(data_file).read())
         except:
-            exceptions_in_mutate_call += 1
+            exceptions_in_mutate_call.append(data_file)
             continue
 
+        # Get Gold-code
         problem_id = data["meta"]["probid"]
         user_id = data["meta"]["subid"]
         key = 'validation' if problem_id in validation_keys else 'train'
@@ -79,6 +79,7 @@ def generate_training_data(path, validation_users):
             if i[0][0] == '}' or i[0][-1] == '{':
                 candidate[i[1]] = i[0]
 
+        # Add temporary header for compilation
         code_list = dict()
         code_list[-3] = "#include <bits/stdc++.h>"
         code_list[-2] = "#include <string>"
@@ -93,16 +94,37 @@ def generate_training_data(path, validation_users):
             else:
                 code_list[data["lines"][i]["line"]] = data["lines"][i]["code"]
 
+        # First compilation error check
         temp_errors, _ = cpp_compilation_errors("\n".join(code_list.values()), check_problem_path)
         if len(temp_errors) != 0:
-            _error += 1
+            _error.append(data_file)
             continue
+
+        # Delete header
+        del code_list[-1]
+        del code_list[-2]
+        del code_list[-3]
 
         try:
             tokenized_code, name_dict, name_sequence = tokenize("\n".join(code_list.values()))
         except:
-            exceptions_in_mutate_call += 1
+            exceptions_in_mutate_call.append(data_file)
             continue
+
+        # Second compilation error check
+        orig_code = tokens_to_source(tokenized_code, name_dict, False)
+        orig_code = orig_code.replace(';', ';\n')
+        orig_code = orig_code.replace('{', '{\n')
+        orig_code = orig_code.replace('}', '}\n')
+        orig_code_list = orig_code.split('\n')
+        orig_code_list.insert(0, "using namespace std ;")
+        orig_code_list.insert(0, "#include <string>")
+        orig_code_list.insert(0, "#include <bits/stdc++.h>")
+        temp_errors, _ = cpp_compilation_errors("\n".join(orig_code_list), check_problem_path)
+        if len(temp_errors) != 0:
+            _token_error.append(data_file)
+            continue
+
         # Correct pairs
         source = ' '.join(remove_line_numbers(tokenized_code).split())
         if len(source.split()) > 400:
@@ -124,19 +146,23 @@ def generate_training_data(path, validation_users):
             for mod_line, mod_code in zip(data["errors"][iter_i]['mod_line'],
                 data["errors"][iter_i]['mod_code']):
                 temp[mod_line] = mod_code
-
             try:
                 corrupt_program, corrupt_name_dict, _ = tokenize("\n".join(temp.values()), name_dict)
             except:
-                exceptions_in_mutate_call += 1
+                exceptions_in_mutate_call.append(data_file)
                 continue
+
             #source sequence
             corrupt_source = ' '.join(remove_line_numbers(corrupt_program).split())
+            if len(corrupt_source.split()) > 400:
+                large_token += 1
+                continue
+
             #target sequence
             try:
                 target = get_target(corrupt_source.split(), source.split())
-            except :
-                exceptions_in_mutate_call += 1
+            except:
+                exceptions_in_mutate_call.append(data_file)
                 continue
 
             try:
@@ -148,8 +174,17 @@ def generate_training_data(path, validation_users):
                         (corrupt_source, corrupt_name_dict, name_sequence,
                             user_id+"_"+str(iter_i), target)]
 
-    print("Exceptions in mutate() call: {}".format(exceptions_in_mutate_call))
-    print("Total data: {}, Error data: {}".format(count, _error))
+    print("Exceptions in mutate() call: {}".format(len(exceptions_in_mutate_call)))
+    print("\n".join(exceptions_in_mutate_call))
+    print("-----------------------------------------------------")
+    print("Total data: {}".format(count))
+    print("-----------------------------------------------------")
+    print("Error data: {}".format(len(_error)))
+    print("\n".join(_error))
+    print("-----------------------------------------------------")
+    print("Token Error data: {}".format(len(_token_error)))
+    print("\n".join(_token_error))
+    print("-----------------------------------------------------")
     print("Data with too large token size: {}".format(large_token))
     return result
 
